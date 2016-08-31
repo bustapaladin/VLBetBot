c59b2bd4a8494a665d55f3f3dd464421c66a969c
Curtis <curtis@voxelloop.co.uk>
2016-08-31 01:37:35 +0100

    Version 1.4
    -Added web-based client settings editing, currently experimental.
    Disabled by default.
    -Lowered default Base Bet.
    -Added Martingale method. (mode 3)
    -Default mode is now 3. (Martingale)
    -Changed default High Average Multiplier.

--- bot-mk1.js
+++ bot-mk1.js
@@ -10,7 +10,7 @@
 */
 
 //Version
-var version = 1.3;
+var version = 1.4;
 
 //----------Site----------//
 var BustaBit = true; 
@@ -21,13 +21,13 @@ var BustaBit = true;
 var autoBaseBetEnabled = true; 
 // Default: false - Enable/Disable auto base bet. Described below.
 
-var maxLossCount = 4;
+var maxLossCount = 8;
 // Default: 5 - Max amount of loses to account for with autoBaseBetEnabled set to true.
 
 var percentageOfTotal = 100;
 // Default: 100 - Percentage of total balance to use when max loss is hit. (100 = 100%)
 
-var baseBet = 100; 
+var baseBet = 10; 
 // Default: 100 - Manual base bet, only used when autoBaseBetEnabled is false.
 
 var cashOut = 1.35; 
@@ -46,8 +46,8 @@ var useCrashAverage = true;
 var highAverage = 1.85; 
 // Default: 1.85 - Average multiplier to use the highAverageMultiplier.
 
-var highAverageMultiplier = 1.50;
-// Default: 1.30 - Multiplier to use when crash average is above highAverage.
+var highAverageMultiplier = 2.0;
+// Default: 2.0 - Multiplier to use when crash average is above highAverage.
 
 var lowAverage = 1.70;
 // Default: 1.60 - Average multiplier to use the lowAverageMultiplier.
@@ -79,8 +79,11 @@ var randomBreak = 0;
 var clearConsole = true;
 // Default: true - Clear the console after 500 minutes, usefull for running the bot on systems with low RAM.
 
+var experimentalFeatures = false;
+// Default: false - Enable experimental web-based settings.
+
 //----------Modes----------//
-var mode = 1; 
+var mode = 3; 
 
 // 1 = Default - Mode will place a bet at the base amount and base multiplier. On loss, it will raise the bet by 4x and increase the multiplier to a max of 1.33x.
 // 2 = 9x Seeker - Mode will place the base amount and 9x multiplier. On loss, it will raise the bet by mode2multiplyBy until a 10x is reached.
@@ -133,9 +136,29 @@ var lossStreak;
 var alreadyRan;
 var highLow;
 var temptime = 500;
+var jsonInc
+var firstGrab = true;
+
+//JSON Grabber
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
 
 if(startup == true){
-	printStartup();
+    printStartup();
+	jsonInc = randNum(1,999999);
+	startup = false;
 }
 
 if(BustaBit == true){
@@ -151,6 +174,43 @@ else{
 engine.on('game_starting', function(info){
 	currentGameID = info.game_id;
 	var random = randNum(1,100);
+    
+	if(experimentalFeatures == true){
+		getJSON(("https://crossorigin.me/http://vl1.voxelloop.co.uk/bustabit/" + user + "?" + jsonInc),
+		function(err, data) {
+		if (err != null) {
+			console.log("Something went wrong whilst grabbing the JSON: Error " + err);
+			console.log("Stopping bot!");
+			engine.stop();
+		} else {
+			jsonInc += 1;
+		
+			if(firstGrab == true){
+				//Insert stuff only to be grabbed once here
+				firstGrab = false;
+			}
+			
+			autoBaseBetEnabled = data.autoBaseBetEnabled;
+			maxLossCount = data.maxLossCount;
+			percentageOfTotal = data.percentageOfTotal;
+			baseBet = data.baseBet;
+			cashOut = data.cashOut;
+			maxBet = data.maxBet;
+			useCrashAverage = data.useCrashAverage;
+			highAverage = data.highAverage;
+			highAverageMultiplier = data.highAverageMultiplier;
+			waitForBeforeRecoveryEnabled = data.waitForBeforeRecoveryEnabled;
+			waitForBeforeRecovery = data.waitForBeforeRecovery;
+			mode2multiplyBy = data.mode2multiplyBy;
+			mode2waitAfterWin = data.mode2waitAfterWin;
+			maxNegative = data.maxNegative;
+			randomBreak = data.randomBreak;
+			clearConsole = data.clearConsole;
+			mode = data.mode;
+			console.log("User data loaded!");
+		}
+		});
+	}
 	
 	if(game4 != null){
 		gameAverage = (((game1 + game2) + (game3 + game4)) / 4);
@@ -172,7 +232,7 @@ engine.on('game_starting', function(info){
 	}
 	
 	//Auto base bet
-	if(autoBaseBetEnabled == true && lastResult == "WON"  && mode != 3 || firstGame == true && mode != 3){
+	if(autoBaseBetEnabled == true && lastResult == "WON" || firstGame == true){
 		var divider = 100;
 		for (i = 0; i < maxLossCount; i++) {
 			if(mode == 1 || mode == 5){
@@ -182,7 +242,7 @@ engine.on('game_starting', function(info){
 				divider += (100 * Math.pow(mode2multiplyBy, (i + 1)));
 			}
 			if(mode == 3){
-				divider += (100 * Math.pow(12, (i + 1)));
+				divider += (100 * Math.pow(2, (i + 1)));
 			}
 		}
 		baseBet = Math.min(Math.max(1, Math.floor((percentageOfTotal/100) * engine.getBalance() / divider)), maxBet * 100);
@@ -369,6 +429,67 @@ engine.on('game_starting', function(info){
 					placeBet();
 					betPlaced = true;
 				}
+			}
+		}
+		
+		//Gamemode 3 (Martingale)
+		if(mode == 3){
+			
+			//Shutdown on max loss
+			if(lossCount > maxLossCount){
+				console.log("Max loss count reached! Shutting down...");
+				engine.stop();
+			}
+			
+			//Reset cooldown
+			if (resetLoss == true) {
+				if (lossCount == 0) {
+				resetLoss = false;
+			}
+			else {
+				lossCount--;
+				console.log('Waiting a few games! Games remaining: ' + lossCount);
+				return;
+				}
+			}
+			
+			//First Game
+			if(firstGame == true && betPlaced == false){
+				currentBet = baseBet;
+				currentMultiplier = 2;
+				firstGame = false;
+				placeBet();
+				newdate = new Date();
+				timeplaying = ((newdate.getTime() - startTime) / 1000) / 60;
+				betPlaced = true;
+			}
+			
+			//On Lost
+			if(lastResult == "LOST" && betPlaced == false && firstGame == false){
+				
+				if(firstLoss == true){
+					lossBalance = 0;
+					lossStreak = 0;
+					firstLoss = false;
+				}
+				
+				lossStreak++;
+				currentBet *= 2;
+				currentMultiplier = 2;
+				console.log("Bet changed to: " + currentBet);
+				placeBet();
+				betPlaced = true;
+			}
+			
+			//On Win
+			if(lastResult == "WON" && betPlaced == false){
+				
+				currentBet = baseBet;
+				currentMultiplier = 2;
+				firstLoss = true;
+			
+				placeBet();
+				betPlaced = true;
 			}
 		}
 	}
